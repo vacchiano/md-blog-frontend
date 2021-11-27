56345deb5caa07bb0f6ebe258df819ade080c686
Dom Vacchiano <djvacchiano@gmail.com>
2021-11-27 01:27:33 -0600

    setup basic routing

---
+++ notes.md
@@ -0,0 +1,9 @@
+## Markdown Blog
+
+Creaate posts
+Update posts
+Delete posts
+List posts
+Retrieve posts (markdown)
+
+CRUD - List
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import {
+  Container,
+  Menu,
+} from 'semantic-ui-react'
+
+const Navbar = () => (
+  <div>
+    <Menu fixed='top' inverted>
+      <Container>
+        <Menu.Item as='a' header>
+          React Markdown Blog
+        </Menu.Item>
+        <NavLink to='/'><Menu.Item as='li'>Posts</Menu.Item></NavLink>
+        <NavLink to='/create'><Menu.Item as='li'>Create Post</Menu.Item></NavLink>
+      </Container>
+    </Menu>
+
+    
+  </div>
+)
+
+export default Navbar
